d8e10fa97afa431ccbf66c94c18202efc1fe614c
Sanjid habib <sanjid.habib@gmail.com>
2019-05-02 19:48:03 +0600

    #

--- resources/views/modules/tasks/form.blade.php
+++ resources/views/modules/tasks/form.blade.php
@@ -79,11 +79,12 @@
     {{--@include('form.input-text',['var'=>['name'=>'due_date','label'=>'Due Date', 'container_class'=>'col-sm-3','params'=>['class'=>'datepicker']]])--}}
     @include('form.input-text',['var'=>['name'=>'due_date','label'=>'Due Date', 'container_class'=>'col-sm-2','params'=>['id'=>'datetimepicker']]])
     @if(isset($task))
-    {{--days_open--}}
-    @include('form.input-text',['var'=>['name'=>'days_open','label'=>'Days Open', 'container_class'=>'col-md-2','params'=>['readonly'=>true]]])
+        {{--days_open--}}
+        @include('form.input-text',['var'=>['name'=>'days_open','label'=>'Days Open', 'container_class'=>'col-md-1','params'=>['readonly'=>true]]])
+        {{--seq--}}
+        @include('form.input-text',['var'=>['name'=>'seq','label'=>'Sequence', 'container_class'=>'col-md-1','params'=>['readonly'=>true]]])
     @endif
-    {{--seq--}}
-    @include('form.input-text',['var'=>['name'=>'seq','label'=>'Sequence', 'container_class'=>'col-md-2']])
+
 </div>
 <div class="clearfix"></div>
 <div class="col-md-6 no-padding">
