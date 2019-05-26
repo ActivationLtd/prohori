0d2a65b3a8ea5cf640a72dc7336e7060095c8e7b
Rifat_Ara_Noor <rifatshara@gmail.com>
2019-05-26 21:39:27 +0600

    font color updated

--- resources/views/emails/templates/prohori-default.blade.php
+++ resources/views/emails/templates/prohori-default.blade.php
@@ -39,7 +39,7 @@
                             <table width="100%" border="0" cellspacing="0" cellpadding="0" align="right"
                                    style=" padding:20px 0;">
                                 <tr>
-                                    <td style="font-family:Arial, Helvetica, sans-serif; font-size:17px; color:#fff; line-height:23px; text-align:center; ">
+                                    <td style="font-family:Arial, Helvetica, sans-serif; font-size:17px; color:#000; line-height:23px; text-align:center; ">
 
                                         <h1 style="margin:0px 0 40px 0; padding:0px; font-weight:normal;  font-size:35px; line-height:normal">
                                             @section('email-content-header')
@@ -53,7 +53,7 @@
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td style="padding-top:40px; font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#fff; line-height:23px; text-decoration:none;  text-align:center;">
+                                    <td style="padding-top:40px; font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#000; line-height:23px; text-decoration:none;  text-align:center;">
                                         <a href="https://www.prohori.com/" style="text-decoration:none; display:inline-block; margin:0 5px" target="_blank"><img src="https://prohori.com/email-images/prohori-icon.png" alt="prohori"  height="24" style=" border:none"> </a>
                                         <a href="https://www.facebook.com/prohori-705589139816728/" style="text-decoration:none; display:inline-block; margin:0 5px" target="_blank"><img src="https://prohori.com/email-images/facebook.png" alt="Facebook"  height="24" style=" border:none"> </a>
                                         <a href="https://twitter.com/prohoriOFFICIAL" style="text-decoration:none; display:inline-block; margin:0 5px" target="_blank"><img src="https://prohori.com/email-images/twitter.png" alt="Twitter"  height="24" style=" border:none"> </a>
@@ -62,10 +62,10 @@
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td style="padding-top:40px; font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#fff; line-height:23px; text-decoration:none;  text-align:center;">
+                                    <td style="padding-top:40px; font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#000; line-height:23px; text-decoration:none;  text-align:center;">
                                         If you receive this email by mistake or in error, delete it or get in contact
                                         with us at <a
-                                                style="font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#fff; text-decoration:none; "
+                                                style="font-family:Arial, Helvetica, sans-serif; font-size:12px; color:#000; text-decoration:none; "
                                                 href="#">letshelp@prohori.com</a>
                                     </td>
                                 </tr>
@@ -79,7 +79,7 @@
     <tr>
         <td>
             <table width="800" border="0" cellspacing="0" cellpadding="0" align="center"
-                   style="background-color:#fff;  ">
+                   style="background-color:#000;  ">
                 <tr>
                     <td style="font-family: proxima_nova, 'Open Sans', 'Lucida Grande', 'Segoe UI', Arial, Verdana, 'Lucida Sans Unicode', Tahoma, 'Sans Serif'; font-size:13px; color:#ccc; line-height:normal; text-align:right; padding:10px 15px ">
                         © {{date('Y')}} prohori
