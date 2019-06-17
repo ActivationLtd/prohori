c46aa4dabc03ec51a3eed18025787d5ca4613a8a
Sanjid habib <sanjid.habib@gmail.com>
2019-06-17 18:23:27 +0600

    #

--- resources/views/modules/clientlocations/form.blade.php
+++ resources/views/modules/clientlocations/form.blade.php
@@ -52,7 +52,7 @@
 <div class="clearfix"></div>
 @include('form.select-model',['var'=>['name'=>'client_id','label'=>'Client','table'=>'clients', 'container_class'=>'col-sm-3']])
 @include('form.select-model',['var'=>['name'=>'operatingarea_id','label'=>'Operating Area','table'=>'operatingareas', 'container_class'=>'col-sm-3']])
-@include('form.select-model',['var'=>['name'=>'clientlocationtype_id','label'=>'Clientlocationtype','table'=>'clientlocationtypes', 'container_class'=>'col-sm-3']])
+@include('form.select-model',['var'=>['name'=>'clientlocationtype_id','label'=>'Clientlocation Type','table'=>'clientlocationtypes', 'container_class'=>'col-sm-3']])
 <div class="clearfix"></div>
 
 {{-- ******************* Form ends *********************** --}}
